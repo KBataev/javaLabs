c7c17a226953e99d19ce3b51fe7c3652fb060976
bot32 <b5968321b@gmail.com>
2021-03-01 11:22:00 +0300

    add idz1

---
+++ src/by/gsu/pms/StockPriceComparator.java
@@ -0,0 +1,19 @@
+package by.gsu.pms;
+
+import java.util.Comparator;
+
+public class StockPriceComparator implements Comparator<Stock> {
+    @Override
+    public int compare (Stock stock1, Stock stock2) {
+        if( stock1 == stock2){
+            return 0;
+        }
+        if (stock1 == null){
+            return 1;
+        }
+        if(stock2 == null){
+            return -1;
+        }
+        return stock1.compareTo(stock2);
+    }
+}
